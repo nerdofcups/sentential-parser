0a4345e1bee2bc95ee198c416a9c4369396ca5f2
Kelly Mayfield <kmayfield@directv.com>
2016-05-04 11:37:36 -0700

    Add TODO list from Code Review
    Added a TODO list as comments in TT from Code Review with Kirk.

--- TT1_Kelly_ByteArray/src/com/att/tutorial/tt/assignment1/bytearray/TT.java
+++ TT1_Kelly_ByteArray/src/com/att/tutorial/tt/assignment1/bytearray/TT.java
@@ -1,5 +1,10 @@
 package com.att.tutorial.tt.assignment1.bytearray;
 
+// many formulas can be inputted through the command line arguments
+//remove reset method
+//have separate threads for each unit test
+//test that a new thread is made 
+
 import java.io.InputStream;
 
 import com.att.tutorial.term.assignment1.bytearray.Term;
