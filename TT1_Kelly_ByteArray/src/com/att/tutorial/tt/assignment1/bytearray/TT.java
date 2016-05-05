e946dcb8f63e237adaf2aef01195ee48c0563e8e
Kelly M <kmayfield@directv.com>
2016-05-05 13:03:49 -0700

    Change TT.java to run strings
    Altered TT.java so that it takes equation inputs as arguments
    and each equation will spin off a new thread.

--- TT1_Kelly_ByteArray/src/com/att/tutorial/tt/assignment1/bytearray/TT.java
+++ TT1_Kelly_ByteArray/src/com/att/tutorial/tt/assignment1/bytearray/TT.java
@@ -1,20 +1,37 @@
 package com.att.tutorial.tt.assignment1.bytearray;
 
+import java.io.ByteArrayInputStream;
+
 // many formulas can be inputted through the command line arguments
 //remove reset method
 //have separate threads for each unit test
 //test that a new thread is made 
 
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import com.att.tutorial.term.assignment1.bytearray.Term;
 
-public class TT {
+public class TT implements Runnable {
 
+	String inputArgs;
+	
+	public TT(String s) {
+		
+		inputArgs = s;
+		
+	}
+	
 	public static void main(String[] args) {
+		
 		try {
 			
-			tt(System.in);			
+			for (String s: args) {
+			
+				Runnable r = new TT(s);
+				new Thread(r).start();
+				
+			}
 			
 		} catch (Exception e ) {
 			
@@ -22,6 +39,14 @@ public class TT {
 			
 		}
 
+	}
+	
+	@Override
+	public void run() {
+		
+		ByteArrayInputStream stream = new ByteArrayInputStream(inputArgs.getBytes(StandardCharsets.UTF_8));
+		tt(stream);
+		
 	}
 	
 	private static void tt(InputStream stream) {
@@ -44,5 +69,11 @@ public class TT {
 		}
 		
 	}
+
+	//@Override
+	//public void run() {
+		// TODO Auto-generated method stub
+		
+	//}
 
 }
